8369113fd014e8f9efa84e89573e42d151203875
AnaMontana10 <42219647+AnaMontana10@users.noreply.github.com>
2019-05-23 11:45:02 -0500

    Add files via upload

---
+++ MontanaAna_final_15.mk
@@ -0,0 +1,11 @@
+MontanaAna_final_15.pdf: MontanaAna_final_15.py final15.dat
+	python MontanaAna_final_15.py
+
+MontanaAna_final_15.png: MontanaAna_final_15.py final15.dat
+	python MontanaAna_final_15.py
+
+final15.dat: MontanaAna_final_15.x
+	./MontanaAna_final_15.x
+
+MontanaAna_final_15.x: MontanaAna_final_15.cpp
+	g++ MontanaAna_final_15.cpp -o MontanaAna_final_15.x 
---
+++ MontanaAna_final_15.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+
+datos = np.genfromtxt("final15.dat")
+
+tiempo = datos[:,0]
+x = datos[:,1]
+y = datos[:,2]
+
+
+plt.figure()
+plt.plot(x,y)
+plt.grid()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("MontanaAna_final_15.pdf")
+plt.savefig("MontanaAna_final_15.png")
