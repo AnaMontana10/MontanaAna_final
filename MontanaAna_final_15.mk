4db8bdba57108e80718f0d76117b39ee5acc0ea8
AnaMontana10 <42219647+AnaMontana10@users.noreply.github.com>
2019-05-23 11:44:42 -0500

    Delete MontanaAna_final_15.mk

--- MontanaAna_final_15.mk
+++
@@ -1,8 +0,0 @@
-MontanaAna_final_15.pdf: MontanaAna_final_15.py final15.dat
-	python MontanaAna_final_15.py
-
-final15.dat: MontanaAna_final_15.x
-	./MontanaAna_final_15.x
-
-MontanaAna_final_15.x: MontanaAna_final_15.cpp
-	g++ MontanaAna_final_15.cpp -o MontanaAna_final_15.x 
